2e13eb124953f19559fc3150c9f27995f6cc9dd6
josediaz868 <146389162+josediaz868@users.noreply.github.com>
2023-11-01 11:44:28 -0600

    Add files via upload

---
+++ portafolio/app.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+      <header>
+        <h1>Bienvenido a mi portafolio</h1>
+      </header>
+      <main>
+        <Proyecto
+          v-for="proyecto in proyectos"
+          :key="proyecto.id"
+          :proyecto="proyecto"
+        ></Proyecto>
+      </main>
+    </div>
+  </template>
+  
+  <script>
+  import Proyecto from "./components/Proyecto.vue";
+  
+  export default {
+    name: "App",
+    components: {
+      Proyecto
+    },
+    data() {
+      return {
+        proyectos: [
+          { id: 1, titulo: "Proyecto 1", descripcion: "Descripción del proyecto 1" },
+          { id: 2, titulo: "Proyecto 2", descripcion: "Descripción del proyecto 2" },
+          // ... más proyectos
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  /* Estilos globales para tu aplicación */
+  </style>
+  
---
+++ portafolio/proyecto.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="proyecto">
+      <h2>{{ proyecto.titulo }}</h2>
+      <p>{{ proyecto.descripcion }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["proyecto"]
+  };
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para el componente Proyecto */
+  </style>
+  
